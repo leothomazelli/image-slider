262c163dd1361c9d6d20d31aef3952e6db461495
leonardo <leonardothomazellif@gmail.com>
2023-08-23 20:25:12 -0300

    uploading project's other half

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { SlideInterface } from './imageSlider/types/slide.interface';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'sliderPractice';
+  slides: SlideInterface[] = [
+    { url: 'assets/gelato.jpg', title: 'gelato' },
+    { url: 'assets/grilled.jpg', title: 'grilled' },
+    { url: 'assets/hamburguer.jpg', title: 'hamburguer' },
+    { url: 'assets/hotdogs.jpg', title: 'hotdogs' },
+    { url: 'assets/japanese.jpg', title: 'japanese' },
+  ];
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImageSliderModule } from './imageSlider/image-slider.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule, AppRoutingModule, ImageSliderModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
